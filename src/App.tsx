2dd53131f58220fe77da862859b924330ccdf893
Vanessa Brazuna <brazuna.nessa@gmail.com>
2023-02-10 12:40:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import ReactFlow, { Background, Controls, Node, ConnectionMode } from "reactflow";
+import { zinc } from 'tailwindcss/colors' 
+import "reactflow/dist/style.css";
+
+import { Square } from './components/nodes/Square'
+
+const NODE_TYPES = {
+  square: Square,
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {},
+  },
+] satisfies Node[]
+
+function App() {
+  return (
+    <div className='w-screen h-screen'>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={INITIAL_NODES}
+        connectionMode={ConnectionMode.Loose}
+      >
+
+        <Background 
+          gap={12}
+          size={2}
+          color={zinc[200]}
+        />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default App;
